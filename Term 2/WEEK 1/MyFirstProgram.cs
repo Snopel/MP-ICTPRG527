79ecd0fc74d7f7cd3acfe07064bb43d3c435c015
Snopel <n.a.balliro95@gmail.com>
2021-04-27 11:39:50 +1000

    Initial Commit

---
+++ Term 2/WEEK 1/MyFirstProgram.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace MyFirstProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 0;
+            int num2 = 0;
+            int op = 0;
+
+            Console.WriteLine("Enter a number: ");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter a second number: ");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Select an operator.\n 1: + | 2: - | 3: * | 4: / ");
+            op = Convert.ToInt32(Console.ReadLine());
+            if(op == 1)
+            {
+                Console.WriteLine("Your numbers added together is " + (num1 + num2));
+            } else if (op == 2)
+            {
+                Console.WriteLine("Your numbers minused is " + (num1 - num2));
+            } else if (op == 3)
+            {
+                Console.WriteLine("Your numbers multiplied is " + (num1 * num2));
+            } else if (op == 4)
+            {
+                Console.WriteLine("Your numbers divided is " + (num1 - num2));
+            } else
+            {
+                Console.WriteLine("INVALID");
+            }
+        }
+    }
+}
---
+++ Term 2/WEEK 1/MyName/MyName/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+/* Program by: Nicholas Balliro
+ * ID: s1509092
+ * Written: 23/04/2021
+ * Purpose: Written for Topic 1 Assessment.
+ *          Q1 to 4 have been commented out
+ *          Q7 is uncommented and runnable to evaluate the required task.
+ */
+namespace MyName
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            //Q1
+            Console.WriteLine("Nicholas\n");
+            Console.WriteLine();
+            //Q2
+            Console.WriteLine("Nicholas Balliro\n12 Acacia Way\nWallan\nVIC\n3756");
+            Console.WriteLine();
+            //Q3
+            Console.WriteLine("   X   \n\n  XXX  \n XXXXX \nXXXXXXX\n   X   ");
+            Console.WriteLine();
+            //Q4
+            Console.WriteLine("  *  \n * * \n* * *\n * * \n  *  ");
+            Console.WriteLine();
+            */
+
+            //Q7 (Submittable)
+            Console.WriteLine(" _____________________________________________________\n"
+                + "|  *** SNOPELNET ***                                   |\n"
+                + "|                                                      |\n"
+                + "|  Nicholas Balliro                                    |\n"
+                + "|  n.balliro@snopelnet.com.au                          |\n"
+                + "|  +61 0432 123 456                                    |\n"
+                + "|                                                      |\n"
+                + "|                          Where Dreams come to life!  |\n"
+                + " _____________________________________________________\n");
+        }
+    }
+}
