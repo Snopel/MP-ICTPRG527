e0abc1eb9e8d39549a41f095988f2d61bcc65bed
Snopel <n.a.balliro95@gmail.com>
2021-05-07 12:29:42 +1000

    Attempted and completed some more activities for this week

---
+++ Term 2/WEEK 3/Furniture/Furniture/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace Furniture
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Wood prices
+            int pine = 100;
+            int oak = 225;
+            int mahogany = 310;
+            //Final table value
+            int table = 0;
+            //Check if the input is valid for second switch statement
+            bool valid = true;
+
+            Console.WriteLine("Welcome to Snopel Furniture!");
+            Console.Write("Please enter your table of choice!\n1: Pine | 2: Oak | 3: Mahogany\n\nChoice: ");
+            int choice = Convert.ToInt32(Console.ReadLine());
+            //Switch statement for each choice value
+            switch (choice)
+            {
+                case 1:
+                    Console.WriteLine("\nYou've selected Pine wood!\nCost: $" + pine);
+                    table = pine;
+                    break;
+                case 2:
+                    Console.WriteLine("\nYou've selected Oak wood!\nCost: $" + oak);
+                    table = oak;
+                    break;
+                case 3:
+                    Console.WriteLine("\nYou've selected Mahogany wood!\nCost: $" + mahogany);
+                    table = mahogany;
+                    break;
+                default:
+                    Console.WriteLine("\nYou've made an incorrect selection.");
+                    valid = false;
+                    break;
+            }
+            //If the default case was not triggered
+            if (valid == true)
+            {
+                Console.Write("\nPlease choose a table size!\n1: Small | 2: Large\n\nChoice: ");
+                int size = Convert.ToInt32(Console.ReadLine());
+
+                switch (size)
+                {
+                    case 1:
+                        Console.WriteLine("\nYou've selected Small! No extra charge.\nCost: $" + table);
+                        break;
+                    case 2:
+                        Console.WriteLine("\nYou've selected Large! $35 extra charge.\nCost: $" + (table + 35));
+                        break;
+                    default:
+                        Console.WriteLine("\nYou've made an incorrect selection.");
+                        break;
+                }
+            }
+
+            Console.WriteLine("\nThank you for shopping with us!");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Term 2/WEEK 3/Lawn/Lawn/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace Lawn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int length = 0;
+            int width = 0;
+            int lawn = 0;
+            int fee = 0;
+            const int SEASON = 20;
+            int seasonFee = 0;
+            int choice = 0;
+
+            Console.WriteLine("Welcome to Jim's Mowing service!");
+            Console.WriteLine("We offer a seasonal 20 week mowing fee!");
+            //Grabbing length and width of the lawn
+            Console.Write("\nPlease enter the LENGTH of the lawn (m): ");
+            length = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\nPlease enter the WIDTH of the lawn (m): ");
+            width = Convert.ToInt32(Console.ReadLine());
+            //Calculating the lawn area
+            lawn = length * width;
+            
+            //Checking the lawn's area and assigning the appropriate fee
+            if (lawn < 400)
+            {
+                fee = 25;
+            } 
+            else if (lawn >= 400 && lawn <= 600)
+            {
+                fee = 35;
+            }
+            else if (lawn > 600)
+            {
+                fee = 50;
+            }
+            
+            //Calculating the seasonal fee
+            seasonFee = fee * SEASON;
+
+            //Displaying the appropriate information to the user
+            Console.WriteLine(
+                "\nYour lawn is " + lawn + " square meters.\n" +
+                "\nYour weekly mowing fee is $" + fee +
+                "\nThe seasonal fee over 20 weeks is $" + seasonFee);
+            //Prompt the user for payment either once, twice or weekly
+            Console.WriteLine("\nHow would you like to pay?\n" +
+                "(1: Whole Fee || 2: Two Payments || 3: Weekly)");
+            choice = Convert.ToInt32(Console.ReadLine());
+
+            //Decide what the payment will be based on the Choice variable
+            switch (choice)
+            {
+                case 1:
+                    Console.WriteLine("You have chosen to pay the whole fee at once.");
+                    Console.WriteLine("The final fee today will be $" + seasonFee);
+                    break;
+                case 2:
+                    Console.WriteLine("You have chosen to pay in two payments.");
+                    Console.WriteLine("There will be 2 payments of $" + ((seasonFee / 2) + 5) + " including a $5 service charge per payment.");
+                    break;
+                case 3:
+                    Console.WriteLine("You have chosen to pay weekly.");
+                    Console.WriteLine("There will be a payment of $" + ((seasonFee / 20) + 3) + " including a $3 service charge per payment every week.");
+                    break;
+                default:
+                    Console.WriteLine("You have made an invalid selection.");
+                    break;
+            }
+            Console.WriteLine("\n\nThank you for choosing Jim's Mowing!");
+
+            Console.ReadLine();
+        }
+    }
+}
