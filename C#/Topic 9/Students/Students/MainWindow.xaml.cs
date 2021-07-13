f58783d49fb426a9d100b08883faec6db39a410d
Snopel <n.a.balliro95@gmail.com>
2021-07-13 11:54:09 +1000

    Began new term and started on OOP

---
+++ C#/Topic 9/Students/Students/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Students
+{
+
+    public partial class MainWindow : Window
+    {
+        Student student = new Student();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnTxt_Click(object sender, RoutedEventArgs e)
+        {
+            txtBox.Text = student.displayStudent();
+        }
+
+        private void phoneTxt_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            student.setPhone(phoneTxt.Text);
+        }
+
+        private void studNoTxt_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            student.setStudNo(studNoTxt.Text);
+        }
+
+        private void nameTxt_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            student.setName(nameTxt.Text);
+        }
+    }
+}
---
+++ C#/Topic 9/Students/Students/Student.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Students
+{
+    class Student
+    {
+        private String name;
+        private String phone;
+        private String studNo;
+
+        public Student()
+        {
+            name = "";
+            phone = "";
+            studNo = "";
+        }
+
+        public Student(String a, String b, String c)
+        {
+            name = a;
+            phone = b;
+            studNo = c;
+        }
+
+        //Setters
+        public void setName(String x)
+        {
+            name = x;
+        }
+
+        public void setPhone(String x)
+        {
+            phone = x;
+        }
+
+        public void setStudNo(String x)
+        {
+            studNo = x;
+        }
+
+        //Getters
+        public String getName()
+        {
+            return name;
+        }
+
+        public String getPhone()
+        {
+            return phone;
+        }
+
+        public String getStudNo()
+        {
+            return studNo;
+        }
+
+        //Display Method
+        public String displayStudent()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("Name: ");
+            sb.Append(name);
+            sb.Append("\nPhobe: ");
+            sb.Append(phone);
+            sb.Append("\nStudNo: ");
+            sb.Append(studNo);
+
+            return sb.ToString();
+        }
+    }
+}
+
